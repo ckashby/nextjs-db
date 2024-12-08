ba57dde87649799954726bf71ea0bcada016ecdc
C K Ashby <clay.ashby@gmail.com>
2024-12-07 21:58:43 -0800

    remove svgs  from dashboard homepage

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,9 +4,6 @@ export default function Page() {
     return (
         <>
         <h1>Dashboard Home</h1>
-        <Image src="typescript.svg" alt="typscript logo" height={100} width={100} />
-        <Image src="svg.svg" alt="svg logo" height={100} width={100} />
-        <Image src="terraform.svg" alt="terraform logo" height={100} width={100} />
         </>
     )
 }
