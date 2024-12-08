2556c622468f42413151f48889cde18071c924af
C K Ashby <clay.ashby@gmail.com>
2024-12-07 22:15:00 -0800

    add fetchrevenue and <RevenueChart />

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,10 +2,13 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue } from '@/app/lib/data';
 
 import Image from "next/image";
 
-export default function Page() {
+export default async function Page() {
+    const revenue = await fetchRevenue();
+
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -22,7 +25,7 @@ export default function Page() {
         /> */}
             </div>
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-                {/* <RevenueChart revenue={revenue}  /> */}
+                <RevenueChart revenue={revenue}  />
                 {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
             </div>
 
