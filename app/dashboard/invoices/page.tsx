d7b69473e47dd2a3d3d488cea4ca3e41ff392fba
C K Ashby <clay.ashby@gmail.com>
2024-12-07 21:56:22 -0800

    add svgs to invoices page

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,7 +1,13 @@
+import Image from "next/image"
+
 export default function Page() {
     return (
         <>
             <h2>Invoices Page</h2>
+
+            <Image src="/typescript.svg" alt="typscript logo" height={100} width={100} />
+            <Image src="/svg.svg" alt="svg logo" height={100} width={100} />
+            <Image src="/terraform.svg" alt="terraform logo" height={100} width={100} />
         </>
     )
 }
