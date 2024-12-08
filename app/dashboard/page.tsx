2ac625aebc310cef51c070dc15031476ae00978b
C K Ashby <clay.ashby@gmail.com>
2024-12-07 22:01:51 -0800

    update imports for dashboard/page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,14 @@
+import { Card } from '@/app/ui/dashboard/cards';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+
 import Image from "next/image";
 
 export default function Page() {
     return (
-        <>
+        <main>
         <h1>Dashboard Home</h1>
-        </>
+        </main>
     )
 }
