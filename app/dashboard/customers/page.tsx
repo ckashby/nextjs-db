78f766e650401c3e3f232776a6b9ae32e4fc312d
C K Ashby <clay.ashby@gmail.com>
2024-12-08 09:55:08 -0800

    Add icons to dashboard/customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image"
+
 export default function Page() {
     return (
         <>
             <h2>Customers Page</h2>
+
+            <Image src="/typescript.svg" alt="typscript logo" height={100} width={100} />
+            <Image src="/svg.svg" alt="svg logo" height={100} width={100} />
+            <Image src="/terraform.svg" alt="terraform logo" height={100} width={100} />
+            
         </>
     )
 }
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -4,7 +4,6 @@ export default function Page() {
     return (
         <>
             <h2>Invoices Page</h2>
-            
         </>
     )
 }
